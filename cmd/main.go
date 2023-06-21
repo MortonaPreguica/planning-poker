aa7c3e53a195c2f69ea52ef04f1405d89bd521c3
Raul Rezende <raul.rezende@libercapital.com.br>
2023-06-21 20:59:55 -0300

    teste

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
+type Player struct {
+	playerId string
+	nota     int8
+	nome     string
+}
+
+var state []Player
 
 func main() {
-	fmt.Print("First Code")
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+
+		fmt.Println("connected:", s.ID())
+
+		s.Emit("teste")
+		return nil
+	})
+
+	go server.Serve()
+	defer server.Close()
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./assets")))
+	log.Println("Serving at localhost:8000...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
